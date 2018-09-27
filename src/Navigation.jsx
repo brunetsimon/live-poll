08d99dc47274000471b3d82262029fc365d284e7
brunetsimon <sbrunet3@gmail.com>
2018-09-27 14:25:14 +0200

    Remove code from Navigation.jsx
    Remove code from Navigation.jsx

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -3,15 +3,14 @@ import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button} from 'material-ui';
 
 const Navigation = () =>
-  <AppBar position="static">
-    <Toolbar>
-      <Typography variant="title" color="inherit">
-        Electromobility LivePoll
-      </Typography>
-      <Button component={Link} to="/client" color="inherit">Client</Button>
-      <Button component={Link} to="/server" color="inherit">Server</Button>
-      {/* <Button component={Link} to="/admin" color="inherit">Admin</Button> */}
-    </Toolbar>
-  </AppBar>
+
+// Requirement: We need an AppBar with a Title and at least 3 buttons.
+// 1. A Client button which links to /client
+// 2. A Server button which links to /server
+// 3. An Admin button which links to /admin
+
+// Suggestion: Have a look here https://material-ui.com/demos/app-bar/ to find out how to use components like AppBar, Toolbar, Button, etc...
+
+// code goes here.
 
 export default Navigation;
