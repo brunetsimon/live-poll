6299c209576062be48fe66ba3d41118f86acbfb7
brunetsimon <sbrunet3@gmail.com>
2018-10-16 08:02:00 +0200

    eslint-disable no-unused-vars

--- src/Result.jsx
+++ src/Result.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
 import { withStyles, Paper, Icon, Typography } from 'material-ui';
 import ratingsConst from './contants';
