0cdd7eb3b3df3d85b81070639a08981afd7bb4b5
brunetsimon <sbrunet3@gmail.com>
2018-10-16 08:03:22 +0200

    eslint-disable no-unused-vars

--- src/VotingClientPage.jsx
+++ src/VotingClientPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import VotingClientSelector from './VotingClientSelector';
