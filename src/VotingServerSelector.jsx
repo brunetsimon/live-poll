a4c2ac9fd4ab2c4dad41ca2b0a7f6158ac76efa4
brunetsimon <sbrunet3@gmail.com>
2018-10-23 14:36:51 +0200

    Update VotingServerSelector.jsx
    with comments...

--- src/VotingServerSelector.jsx
+++ src/VotingServerSelector.jsx
@@ -1,32 +1,40 @@
-import React, { Component } from 'react';
-import { Input, Paper, Button, withStyles, Typography, FormControl, FormHelperText } from 'material-ui';
-import database from './database.js';
+/* eslint-disable no-unused-vars */
+import React, { Component } from "react";
+import {
+  Input,
+  Paper,
+  Button,
+  withStyles,
+  Typography,
+  FormControl,
+  FormHelperText
+} from "material-ui";
+import database from "./database.js";
 
 const styles = {
   formContainer: {
-    padding: "2rem",
+    padding: "2rem"
   },
   contentContainer: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     flex: 1,
-    flexFlow: "column",
+    flexFlow: "column"
   },
   button: {
     display: "block",
-    marginTop: "10px",
+    marginTop: "10px"
   }
-}
+};
 
 class VotingServerSelector extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       pollId: "",
-      errorMsg: "",
+      errorMsg: ""
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -34,43 +42,43 @@ class VotingServerSelector extends Component {
   }
 
   handleInputChange(event) {
-    this.setState({pollId: event.target.value, errorMsg: ""});
+    //store pollId value in the local state. Google react + setState.
+    // Also hide the error if there was one.
   }
 
   handleSubmit(event) {
     event.preventDefault();
 
-    let ref = "/polls/"+this.state.pollId;
-    this.setState({validateStatus: 'validating', errorMsg: ""})
-    database.ref(ref).once('value', (snap) => {
+    // At which path to look in the database
+    let ref = "/polls/" + this.state.pollId;
+
+    //Hide possible previous error.
+    //here
+
+    // Check the database.
+    database.ref(ref).once("value", snap => {
       if (snap.val() != null) {
-        this.props.history.push(`/server/${this.state.pollId}`);
+        // snap (which contain our poll result exists. Let's navigate to our poll.)
+        // code here.
       } else {
-        this.setState({showError : true, errorMsg: "Wrong ID"});
+        // There isn't a poll with this id. Show an error message
+        // code here.
       }
     });
   }
   render() {
-
     const { classes } = this.props;
-
-    return(
+    return (
       <div className={classes.contentContainer}>
         <Paper className={classes.formContainer}>
-          <Typography variant="headline" gutterBottom="true" >Enter your poll number</Typography>
-          <form onSubmit={this.handleSubmit}>
-            <FormControl error={this.state.errorMsg !== ""} fullWidth>
-              <Input id="pollId" value={this.state.pollId} onChange={this.handleInputChange} />
-              <FormHelperText id="name-error-text">{this.state.errorMsg}</FormHelperText>
-            </FormControl>
-            <Button type='submit' color="primary" variant="raised" className={classes.button} fullWidth>
-              Go!
-            </Button>
-          </form>
+          <Typography variant="headline" gutterBottom="true">
+            Enter your poll number
+          </Typography>
+          <form onSubmit={this.handleSubmit}>Code here!</form>
         </Paper>
       </div>
     );
   }
-};
+}
 
 export default withStyles(styles)(VotingServerSelector);
