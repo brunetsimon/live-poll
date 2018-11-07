2e5df2622b928c4dfd522291c6b2545f63160005
brunetsimon <sbrunet3@gmail.com>
2018-11-07 15:50:12 +0100

    Update VotingServer.jsx

--- src/VotingServer.jsx
+++ src/VotingServer.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import database from './database.js';
-import { Typography, withStyles } from 'material-ui';
-import ReactChartkick, { BarChart } from 'react-chartkick';
-import Chart from 'chart.js';
+import React, { Component } from "react";
+import database from "./database.js";
+import { Typography, withStyles } from "material-ui";
+import ReactChartkick, { BarChart } from "react-chartkick";
+import Chart from "chart.js";
 
 ReactChartkick.addAdapter(Chart);
 
@@ -14,47 +14,60 @@ const styles = {
     flex: 1,
     flexFlow: "column",
     maxWidth: "900px",
-    margin: "0 auto",
+    margin: "0 auto"
   },
   contentContainer: {
-    height: "100%",
+    height: "100%"
   },
   header: {
     textAlign: "center",
     margin: "30px auto"
   }
-}
+};
 
 class VotingServer extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       countArray: [],
-    }
+      };
+    // Define a local state call countArray which is an array.
+    // See https://reactjs.org/docs/state-and-lifecycle.html#adding-local-state-to-a-class
+
+    //countArray[0] has the number of vote for the "Love" feeling.
+    //countArray[3] for the "bad"
   }
 
   componentDidMount() {
-    let ref = "/polls/"+this.props.match.params.pollId+"/ratingCount";
-    database.ref(ref).on('value', (snap) => {
+    // componentDidMount is called once at startup. That's a great place to initialize our
+    // function that listen to database changes.
+
+    // ref is the path to the total count.
+    let ref = "/polls/" + this.props.match.params.pollId + "/ratingCount";
+    var countRef = database.ref(ref);
+    countRef.on('value', (snap) =>
+    {
       this.setState({"countArray": snap.val()});
     });
+
+    // Here we need to put the function that will listen to database updates in the "ref" location
+    // and we need to store that value locally in "countArray"
   }
 
   render() {
-      const { classes } = this.props;
+    const { classes } = this.props;
 
-      return (
-        <div className={classes.contentContainer}>
-          <div className={classes.header}>
-            <Typography variant="headline">To vote connect to:</Typography>
-            <Typography variant="subheading">Poll ID = {this.props.match.params.pollId}</Typography>
-          </div>
-          <div className={classes.voteContainer}>
-            <BarChart download={this.props.match.params.pollId} data={[["Love", this.state.countArray[0]], ["Good", this.state.countArray[1]], ["Ok", this.state.countArray[2]], ["Bad", this.state.countArray[3]]]} />
-          </div>
+    return (
+      <div className={classes.contentContainer}>
+        <div className={classes.header}>
+          <Typography> <center> Voting Results</center> </Typography>
+          <Typography> <center> Current Poll ID: {this.props.match.params.pollId}</center> </Typography>
         </div>
-      );
+        <div className={classes.voteContainer}>
+          <BarChart data={[["Love"	, this.state.countArray[0]], ["Good", this.state.countArray[1]], ["Ok", this.state.countArray[2]], ["Bad", this.state.countArray[3]]]} />
+        </div>
+      </div>
+    );
   }
 }
 
