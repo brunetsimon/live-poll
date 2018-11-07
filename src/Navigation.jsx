08c8017e7514803d7f4c56779926d1012eadc8e3
bassemfarag <eng.bassem.hassan@hotmail.com>
2018-11-07 16:15:57 +0100

    Update Navigation.jsx
    First attempt

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,18 +1,166 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/jsx-no-comment-textnodes */ 
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button} from 'material-ui';
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import {
+  withStyles,
+  MuiThemeProvider,
+  createMuiTheme
+} from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+import blue from "@material-ui/core/colors/blue";
+import { Link } from "react-router-dom";
 
-const Navigation = () =>
+const styles = theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  margin: {
+    margin: theme.spacing.unit
+  },
+  client: {
+    boxShadow: "yes",
+    textTransform: "none",
+    fontSize: 16,
+    padding: "6px 12px",
+    border: "1px solid",
+    backgroundColor: "#1976d2",
+    borderColor: "#9e9e9e",
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(","),
+    "&:hover": {
+      backgroundColor: "#0069d9",
+      borderColor: "#9e9e9e"
+    },
+    "&:active": {
+      boxShadow: "none",
+      backgroundColor: "#0062cc",
+      borderColor: "#005cbf"
+    },
+    "&:focus": {
+      boxShadow: "0 0 0 0.2rem rgba(255,255,255,.5)"
+    }
+  },
+  admin: {
+    boxShadow: "yes",
+    textTransform: "none",
+    fontSize: 16,
+    padding: "6px 12px",
+    border: "1px solid",
+    backgroundColor: "#1976d2",
+    borderColor: "#9e9e9e",
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(","),
+    "&:hover": {
+      backgroundColor: "#0069d9",
+      borderColor: "#9e9e9e"
+    },
+    "&:active": {
+      boxShadow: "none",
+      backgroundColor: "#0062cc",
+      borderColor: "#005cbf"
+    },
+    "&:focus": {
+      boxShadow: "0 0 0 0.2rem rgba(255,255,255,.5)"
+    }
+  },
+  server: {
+    boxShadow: "yes",
+    textTransform: "none",
+    fontSize: 16,
+    padding: "6px 12px",
+    border: "1px solid",
+    backgroundColor: "#1976d2",
+    borderColor: "#9e9e9e",
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(","),
+    "&:hover": {
+      backgroundColor: "#0069d9",
+      borderColor: "#9e9e9e"
+    },
+    "&:active": {
+      boxShadow: "none",
+      backgroundColor: "#0062cc",
+      borderColor: "#005cbf"
+    },
+    "&:focus": {
+      boxShadow: "0 0 0 0.2rem rgba(255,255,255,.5)"
+    }
+  }
+});
 
-// Requirement: We need an AppBar with a Title and at least 3 buttons.
-// 1. A Client button which links to /client
-// 2. A Server button which links to /server
-// 3. An Admin button which links to /admin
+function CustomizedInputs(props) {
+  const { classes } = props;
 
-// Suggestion: Have a look here https://material-ui.com/demos/app-bar/ to find out how to use components like AppBar, Toolbar, Button, etc...
-<AppBar>
-// code goes here.
-</AppBar>
-export default Navigation;
+  return (
+    <div className={classes.container}>
+      <Link to="/client" style={{ textDecoration: "none" }}>
+        <Button
+          variant="contained"
+          color="primary"
+          className={classNames(classes.margin, classes.client)}
+        >
+          Homepage
+        </Button>
+      </Link>
+      <Link to="/admin" style={{ textDecoration: "none" }}>
+        <Button
+          variant="contained"
+          color="primary"
+          disableRipple
+          className={classNames(classes.margin, classes.admin)}
+        >
+          Vote Selector
+        </Button>
+      </Link>
+      <Link to="/admin" style={{ textDecoration: "none" }}>
+        <Button
+          variant="contained"
+          color="primary"
+          disableRipple
+          className={classNames(classes.margin, classes.server)}
+        >
+          Vote Display
+        </Button>
+      </Link>
+    </div>
+  );
+}
+
+CustomizedInputs.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(CustomizedInputs);
