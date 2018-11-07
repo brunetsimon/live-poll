1b1b74e9e73bf8554cb4917465abf1c49eb20f15
brunetsimon <sbrunet3@gmail.com>
2018-11-07 16:33:25 +0100

    Update Navigation.jsx

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -4,9 +4,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import {
-  withStyles,
-  MuiThemeProvider,
-  createMuiTheme
+withStyles,
+MuiThemeProvider,
+createMuiTheme
 } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import purple from "@material-ui/core/colors/purple";
@@ -15,154 +15,153 @@ import blue from "@material-ui/core/colors/blue";
 import { Link } from "react-router-dom";
 
 const styles = theme => ({
-  container: {
-    display: "flex",
-    flexWrap: "wrap"
-  },
-  margin: {
-    margin: theme.spacing.unit
-  },
-  client: {
-    boxShadow: "yes",
-    textTransform: "none",
-    fontSize: 16,
-    padding: "6px 12px",
-    border: "1px solid",
-    backgroundColor: "#1976d2",
-    borderColor: "#9e9e9e",
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"'
-    ].join(","),
-    "&:hover": {
-      backgroundColor: "#0069d9",
-      borderColor: "#9e9e9e"
-    },
-    "&:active": {
-      boxShadow: "none",
-      backgroundColor: "#0062cc",
-      borderColor: "#005cbf"
-    },
-    "&:focus": {
-      boxShadow: "0 0 0 0.2rem rgba(255,255,255,.5)"
-    }
-  },
-  admin: {
-    boxShadow: "yes",
-    textTransform: "none",
-    fontSize: 16,
-    padding: "6px 12px",
-    border: "1px solid",
-    backgroundColor: "#1976d2",
-    borderColor: "#9e9e9e",
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"'
-    ].join(","),
-    "&:hover": {
-      backgroundColor: "#0069d9",
-      borderColor: "#9e9e9e"
-    },
-    "&:active": {
-      boxShadow: "none",
-      backgroundColor: "#0062cc",
-      borderColor: "#005cbf"
-    },
-    "&:focus": {
-      boxShadow: "0 0 0 0.2rem rgba(255,255,255,.5)"
-    }
-  },
-  server: {
-    boxShadow: "yes",
-    textTransform: "none",
-    fontSize: 16,
-    padding: "6px 12px",
-    border: "1px solid",
-    backgroundColor: "#1976d2",
-    borderColor: "#9e9e9e",
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"'
-    ].join(","),
-    "&:hover": {
-      backgroundColor: "#0069d9",
-      borderColor: "#9e9e9e"
-    },
-    "&:active": {
-      boxShadow: "none",
-      backgroundColor: "#0062cc",
-      borderColor: "#005cbf"
-    },
-    "&:focus": {
-      boxShadow: "0 0 0 0.2rem rgba(255,255,255,.5)"
-    }
-  }
+container: {
+display: "flex",
+flexWrap: "wrap"
+},
+margin: {
+margin: theme.spacing.unit
+},
+client: {
+boxShadow: "yes",
+textTransform: "none",
+fontSize: 16,
+padding: "6px 12px",
+border: "1px solid",
+backgroundColor: "#1976d2",
+borderColor: "#9e9e9e",
+fontFamily: [
+"-apple-system",
+"BlinkMacSystemFont",
+'"Segoe UI"',
+"Roboto",
+'"Helvetica Neue"',
+"Arial",
+"sans-serif",
+'"Apple Color Emoji"',
+'"Segoe UI Emoji"',
+'"Segoe UI Symbol"'
+].join(","),
+"&:hover": {
+backgroundColor: "#0069d9",
+borderColor: "#9e9e9e"
+},
+"&:active": {
+boxShadow: "none",
+backgroundColor: "#0062cc",
+borderColor: "#005cbf"
+},
+"&:focus": {
+boxShadow: "0 0 0 0.2rem rgba(255,255,255,.5)"
+}
+},
+admin: {
+boxShadow: "yes",
+textTransform: "none",
+fontSize: 16,
+padding: "6px 12px",
+border: "1px solid",
+backgroundColor: "#1976d2",
+borderColor: "#9e9e9e",
+fontFamily: [
+"-apple-system",
+"BlinkMacSystemFont",
+'"Segoe UI"',
+"Roboto",
+'"Helvetica Neue"',
+"Arial",
+"sans-serif",
+'"Apple Color Emoji"',
+'"Segoe UI Emoji"',
+'"Segoe UI Symbol"'
+].join(","),
+"&:hover": {
+backgroundColor: "#0069d9",
+borderColor: "#9e9e9e"
+},
+"&:active": {
+boxShadow: "none",
+backgroundColor: "#0062cc",
+borderColor: "#005cbf"
+},
+"&:focus": {
+boxShadow: "0 0 0 0.2rem rgba(255,255,255,.5)"
+}
+},
+server: {
+boxShadow: "yes",
+textTransform: "none",
+fontSize: 16,
+padding: "6px 12px",
+border: "1px solid",
+backgroundColor: "#1976d2",
+borderColor: "#9e9e9e",
+fontFamily: [
+"-apple-system",
+"BlinkMacSystemFont",
+'"Segoe UI"',
+"Roboto",
+'"Helvetica Neue"',
+"Arial",
+"sans-serif",
+'"Apple Color Emoji"',
+'"Segoe UI Emoji"',
+'"Segoe UI Symbol"'
+].join(","),
+"&:hover": {
+backgroundColor: "#0069d9",
+borderColor: "#9e9e9e"
+},
+"&:active": {
+boxShadow: "none",
+backgroundColor: "#0062cc",
+borderColor: "#005cbf"
+},
+"&:focus": {
+boxShadow: "0 0 0 0.2rem rgba(255,255,255,.5)"
+}
+}
 });
 
 function Navigation(props) {
-  const { classes } = props;
+const { classes } = props;
 
-  return (
-    <div className={classes.container}>
-      <Link to="/" style={{ textDecoration: "none" }}>
-        <Button
-          variant="contained"
-          color="primary"
-          className={classNames(classes.margin, classes.client)}
-        >
-          Homepage
-        </Button>
-      </Link>
-      <Link to="/client" style={{ textDecoration: "none" }}>
-        <Button
-          variant="contained"
-          color="primary"
-          disableRipple
-          className={classNames(classes.margin, classes.admin)}
-        >
-          Vote Selector
-        </Button>
-      </Link>
-      <Link to="/server" style={{ textDecoration: "none" }}>
-        <Button
-          variant="contained"
-          color="primary"
-          disableRipple
-          className={classNames(classes.margin, classes.server)}
-        >
-          Vote Display
-        </Button>
-      </Link>
-    </div>
-  );
+return (
+<div className={classes.container}>
+<Button
+to="/"
+variant="contained"
+color="primary"
+className={classNames(classes.margin, classes.client)}
+>
+Homepage
+</Button>
+<Button
+to="/client"
+variant="contained"
+color="primary"
+disableRipple
+className={classNames(classes.margin, classes.admin)}
+>
+Vote Selector
+</Button>
+<Button
+to="/server"
+variant="contained"
+color="primary"
+disableRipple
+className={classNames(classes.margin, classes.server)}
+>
+Vote Display
+</Button>
+</div>
+);
 }
 
 Navigation.propTypes = {
-  classes: PropTypes.object.isRequired
+classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Navigation);
+
+
