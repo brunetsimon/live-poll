93d6723a63779155d50635a018706fd7c1a6232e
TeamTwoAsp <44838391+TeamTwoAsp@users.noreply.github.com>
2018-11-07 15:05:22 +0100

    Update Vote.jsx
    Implemented voting icons.

--- src/Vote.jsx
+++ src/Vote.jsx
@@ -1,33 +1,41 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/jsx-no-comment-textnodes */
-import React, { Component } from 'react';
-import { IconButton, Icon, withStyles } from 'material-ui';
-import ratingsConst from './contants';
-
+import React, { Component } from "react";
+import { IconButton, Icon, withStyles } from "material-ui";
+import ratingsConst from "./contants";
 
 const styles = {
   iconButton: {
-    display: "block",
+    display: "inline-block",
     height: "16vh",
-    width: "16vh",
+    width: "16vh"
   },
   icon: {
-    fontSize: "80px",
+    fontSize: "120px",
+    //background: "yellow"
   }
-}
+};
 
 class Vote extends Component {
-
   render() {
-    
     const { classes } = this.props;
-    
-    return(
-      <div>
-        // code here. You can remove the div dans replace it with your own code
+
+    return (
+      <div>  
+      
+        <IconButton className={classes.iconButton} onclick="this.props.callbackVote.bind(null, 0)" aria-label="very_satisfied">
+          <Icon className={classes.icon} >sentiment_very_satisfied</Icon>
+        </IconButton> 
+        <IconButton className={classes.iconButton} onclick="this.props.callbackVote.bind(null, 1)" aria-label="satisfied">
+          <Icon className={classes.icon} >sentiment_satisfied</Icon>
+        </IconButton>  
+        <IconButton className={classes.iconButton} onclick="this.props.callbackVote.bind(null, 2)" aria-label="dissatisfied">
+          <Icon className={classes.icon} >sentiment_dissatisfied</Icon>
+        </IconButton> 
+        <IconButton className={classes.iconButton} onclick="this.props.callbackVote.bind(null, 3)" aria-label="very_dissatisfied">
+          <Icon className={classes.icon} >sentiment_very_dissatisfied</Icon>
+        </IconButton>  
       </div>
+
     );
   }
 }
-
 export default withStyles(styles)(Vote);
