491964eb905b65bc5dccae17f73534bcae9208cd
erikstroby <44838237+erikstroby@users.noreply.github.com>
2018-11-07 16:20:18 +0100

    Update Homepage.jsx

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,22 +1,37 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
 import { Link } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Button} from "material-ui";
-import  DeleteIcon  from  '@material-ui/icons/Delete';
-import  CloudUploadIcon  from  '@material-ui/icons/CloudUpload';
-import  KeyboardVoiceICon  from  '@material-ui/icons/KeyboardVoice';
-import  Icon  from  '@material-ui/core/Icon';
-import  SaveIcon  from  '@material-ui/icons/Save';
+import { AppBar, Toolbar, Typography} from "material-ui";
+import  DeleteIcon  from  '@material-ui/icons/Delete';
+import  CloudUploadIcon  from  '@material-ui/icons/CloudUpload';
+import  KeyboardVoiceICon  from  '@material-ui/icons/KeyboardVoice';
+import  Icon  from  '@material-ui/core/Icon';
+import  SaveIcon  from  '@material-ui/icons/Save';
+import  {  withStyles  }  from  '@material-ui/core/styles';
+import classNames  from  "classnames"; 
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
 
-const Homepage = () => (
-  
+const  styles  =  theme  =>  ({
+  button:  {
+    margin:  theme.spacing.unit,
+  },
+}); 
+
+function Homepage(props) {
+  const { classes } = props;
+  return(
   <div align="center" valign="center">
-    <p> &nbsp </p>
-    <h1>Electromobility voting app(webpage)</h1>
+    <p> &nbsp; </p>
+      <p> &nbsp; </p>
+    <h1>ELECTROMOBILITY VOTING APP</h1>
     
-    <Button to="/client" variant="contained" color="primary">Start voting </Button>
-    <Button to="/server" variant="contained"  color="primary">Display a voting graph</Button>
-  </div>
-);
 
-export default Homepage;
+
+    
+    <Button to="/client" variant="contained" className={classes.button} color="primary">Start voting </Button>
+    <Button to="/server" variant="contained" className={classes.button}  color="primary">Display a voting graph</Button>
+  </div>
+  );
+};
+
+export default withStyles(styles)(Homepage);
