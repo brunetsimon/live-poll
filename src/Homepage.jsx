5d5982b465e1342505f41b57f5e868b31df092b7
brunetsimon <sbrunet3@gmail.com>
2018-11-09 08:08:03 +0100

    Update Homepage.jsx

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,14 +1,19 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { Link } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Button} from "material-ui";
+import { AppBar, Toolbar, Typography, Button, withStyles} from "material-ui";
 
+const styles = {
+  title: {
+    marginTop: "2vh",
+  }
+}
 
 function Homepage(props) {
   const { classes } = props;
   return(
   <div>
-    <Typography component="h1" variant="h3" gutterBottom>ELECTROMOBILITY VOTING APP</Typography>
+    <Typography className={classes.title} component="h1" variant="h3" gutterBottom>ELECTROMOBILITY VOTING APP</Typography>
     <Button component={Link} to="/client" variant="raised" color="primary">Start voting </Button>
     <Button component={Link} to="/server" variant="raised" color="primary">Display a voting graph</Button>
   </div>
