0cb17a642b979f8808955734257d1bc5be2f6ee7
brunetsimon <sbrunet3@gmail.com>
2018-11-09 08:05:55 +0100

    Update Homepage.jsx

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,27 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { Link } from "react-router-dom";
-import { AppBar, Toolbar, Typography} from "material-ui";
-import  {  withStyles  }  from  '@material-ui/core/styles';
-import classNames  from  "classnames"; 
-import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
+import { AppBar, Toolbar, Typography, Button} from "material-ui";
 
-const  styles  =  theme  =>  ({
-  button:  {
-    margin:  theme.spacing.unit,
-  },
-}); 
 
 function Homepage(props) {
   const { classes } = props;
   return(
-  <div align="center" valign="center">
-    <p> &nbsp; </p>
-      <p> &nbsp; </p>
-      <Typography component="h1" variant="h3" gutterBottom>ELECTROMOBILITY VOTING APP</Typography>
-    <Button component={Link} to="/client" variant="raised" className={classes.button} color="primary">Start voting </Button>
-    <Button component={Link} to="/server" variant="raised" className={classes.button}  color="primary">Display a voting graph</Button>
+  <div>
+    <Typography component="h1" variant="h3" gutterBottom>ELECTROMOBILITY VOTING APP</Typography>
+    <Button component={Link} to="/client" variant="raised" color="primary">Start voting </Button>
+    <Button component={Link} to="/server" variant="raised" color="primary">Display a voting graph</Button>
   </div>
   );
 };
